6c7a4faa34ff49b4e1ed7ba423d5568f45e42c52
Marcin Sikorski <sikorski.ms@pg.com>
2021-08-25 14:53:08 +0200

    Feat: Implemented basic endpoints

--- src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/entrypoint/PaymentController.java
+++ src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/entrypoint/PaymentController.java
@@ -1,17 +1,44 @@
 package com.marcinsikorski.paymentcrud.payment.infrastructure.entrypoint;
 
+import com.marcinsikorski.paymentcrud.payment.domain.PaymentDTO;
+import com.marcinsikorski.paymentcrud.payment.infrastructure.PaymentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
 public class PaymentController {
 
-    @GetMapping("/healthCheck")
-    public String helloWorld() {
-        return "Hello World!";
+    private final PaymentService paymentService;
+
+    @GetMapping("/{paymentId}")
+    public ResponseEntity<PaymentDTO> getPaymentById(@PathVariable("paymentId") Long paymentId) {
+        PaymentDTO paymentDTO = paymentService.findById(paymentId);
+        return ResponseEntity.status(HttpStatus.OK).body(paymentDTO);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Long> registerPayment(@Valid @RequestBody NewPaymentInput newPaymentInput) {
+        Long paymentId = this.paymentService.savePayment(newPaymentInput);
+        return ResponseEntity.status(HttpStatus.OK).body(paymentId);
+    }
+
+    @PutMapping("/{paymentId}")
+    public ResponseEntity<PaymentDTO> updatePayment(@PathVariable("paymentId") Long paymentId, @Valid @RequestBody ModifiedPaymentInput modifiedPaymentInput) {
+        PaymentDTO paymentDTO = this.paymentService.updatePayment(paymentId, modifiedPaymentInput);
+        return ResponseEntity.status(HttpStatus.OK).body(paymentDTO);
+    }
+
+    @DeleteMapping("/{paymentId}")
+    public ResponseEntity updatePayment(@PathVariable("paymentId") Long paymentId) {
+        this.paymentService.deletePayment(paymentId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(paymentId);
     }
 }
