4d9a7f64ac6e60603e14debbe2e9b7dfc5d42a43
Marcin Sikorski <sikorski.ms@pg.com>
2021-08-25 13:48:30 +0200

    Added comment helping in switch to DB version of PaymentsDataProvider

--- src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/config/PaymentServiceConfiguration.java
+++ src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/config/PaymentServiceConfiguration.java
@@ -10,6 +10,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PaymentServiceConfiguration {
 
+//    possible easy switch to in memory DB version of PaymentDataProvider
+//    @Bean
+//    PaymentService paymentService(PaymentDbDataProviderAdapter paymentDbDataProviderAdapter){
+//        return new PaymentService(paymentDbDataProviderAdapter);
+//    }
+
     @Bean
     PaymentService paymentService(PaymentCSVDataProviderAdapter paymentCSVDataProviderAdapter){
         return new PaymentService(paymentCSVDataProviderAdapter);
