146d8a3b1254652afb1d624e09e3df74944703a7
Marcin Sikorski <sikorski.ms@pg.com>
2021-08-25 12:59:37 +0200

    Implemented update and delete in csv db

--- src/test/java/com/marcinsikorski/paymentcrud/PaymentServiceTests.java
+++ src/test/java/com/marcinsikorski/paymentcrud/PaymentServiceTests.java
@@ -2,6 +2,7 @@ package com.marcinsikorski.paymentcrud;
 
 import com.marcinsikorski.paymentcrud.payment.domain.PaymentDTO;
 import com.marcinsikorski.paymentcrud.payment.infrastructure.PaymentService;
+import com.marcinsikorski.paymentcrud.payment.infrastructure.entrypoint.ModifiedPaymentInput;
 import com.marcinsikorski.paymentcrud.payment.infrastructure.entrypoint.NewPaymentInput;
 import com.marcinsikorski.paymentcrud.payment.infrastructure.repository.PaymentEntity;
 import org.junit.jupiter.api.Assertions;
@@ -9,12 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.util.Currency;
 
 import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Profile({"test"})
 @SpringBootTest
@@ -47,6 +50,34 @@ public class PaymentServiceTests {
         Assertions.assertNotNull(foundDTO.getCurrency(), "Saved currency code should not be null");
         Assertions.assertNotNull(foundDTO.getAmount(),  "Saved amount should not be null");
         Assertions.assertNotNull(foundDTO.getTargetBankAccount(), "Saved target bank account should not be null");
+    }
+
+    @Test
+    public void testDeletePayment() {
+        NewPaymentInput newPaymentInput = getExamplePaymentInput();
+        Long paymentId = paymentService.savePayment(newPaymentInput);
+        paymentService.deletePayment(paymentId);
+        Assertions.assertThrows(ResponseStatusException.class, () -> paymentService.findById(paymentId));
+    }
+
+    @Test
+    public void testUpdatePayment() {
+        NewPaymentInput newPaymentInput = getExamplePaymentInput();
+        Long paymentId = paymentService.savePayment(newPaymentInput);
+        PaymentDTO beforeUpdate = paymentService.findById(paymentId);
+        ModifiedPaymentInput modifiedPaymentInput = ModifiedPaymentInput.builder()
+                .amount(BigDecimal.valueOf(new Double(4350.00)))
+                .currency(Currency.getInstance("USD"))
+                .userId(beforeUpdate.getUserId())
+                .targetBankAccount(beforeUpdate.getTargetBankAccount())
+                .build();
+        PaymentDTO updatedPayment = paymentService.updatePayment(paymentId, modifiedPaymentInput);
+        Assertions.assertEquals(updatedPayment.getPaymentId(), beforeUpdate.getPaymentId(), "Payment id wasn't modified so should be equal");
+        Assertions.assertEquals(updatedPayment.getUserId(), beforeUpdate.getUserId(), "Payment user wasn't modified so should be equal");
+        Assertions.assertEquals(updatedPayment.getTargetBankAccount(), beforeUpdate.getTargetBankAccount(), "Saved target bank account should be equal");
+        Assertions.assertNotEquals(updatedPayment.getCurrency(), beforeUpdate.getCurrency(), "Payment currency was modified so should not be equal");
+        Assertions.assertNotEquals(updatedPayment.getAmount().stripTrailingZeros(), beforeUpdate.getAmount().stripTrailingZeros(), "Amount was modified so should be not equal");
+
     }
 
     private NewPaymentInput getExamplePaymentInput() {
