159cf647cdc14caaf8442b911a5b39a1a73749dd
Marcin Sikorski <sikorski.ms@pg.com>
2021-08-25 17:43:03 +0200

    Improvement error handling and request validation

--- src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/entrypoint/NewPaymentInput.java
+++ src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/entrypoint/NewPaymentInput.java
@@ -2,6 +2,9 @@ package com.marcinsikorski.paymentcrud.payment.infrastructure.entrypoint;
 
 import lombok.*;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.Currency;
 
@@ -11,8 +14,14 @@ import java.util.Currency;
 @AllArgsConstructor
 @Builder
 public class NewPaymentInput {
+    @Min(value = 0L, message = "The value must be positive")
+    @NotNull
     private BigDecimal amount;
+    @Min(value = 0L, message = "User id has to be valid, positive number")
+    @NotNull
     private Long userId;
+    @NotNull
     private Currency currency;
+    @NotEmpty(message = "Target Bank Account cannot be empty")
     private String targetBankAccount;
 }
--- src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/entrypoint/PaymentController.java
+++ src/main/java/com/marcinsikorski/paymentcrud/payment/infrastructure/entrypoint/PaymentController.java
@@ -5,6 +5,7 @@ import com.marcinsikorski.paymentcrud.payment.infrastructure.PaymentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
